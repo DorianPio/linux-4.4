Moxa-Linux/am335x-linux-4.4

net: usb: support cellular module YUGA CLM920-NC5

drivers/net/usb/qmi_wwan.c
@@ -59,6 +59,8 @@ enum qmi_wwan_flags {
 	QMI_WWAN_FLAG_RAWIP = 1 << 0,
 };
 
+static int qmi_wwan_cdc_wdm_manage_power(struct usb_interface *intf, int on);
+
 static void qmi_wwan_netdev_setup(struct net_device *net)
 {
 	struct usbnet *dev = netdev_priv(net);
@@ -213,6 +215,115 @@ static int qmi_wwan_rx_fixup(struct usbnet *dev, struct sk_buff *skb)
 	skb_reset_mac_header(skb);
 	eth_hdr(skb)->h_proto = proto;
 	eth_zero_addr(eth_hdr(skb)->h_source);
+fix_dest:
+	memcpy(eth_hdr(skb)->h_dest, dev->net->dev_addr, ETH_ALEN);
+	return 1;
+}
+
+static int qmi_wwan_bind_shared(struct usbnet *dev, struct usb_interface *intf)
+{
+	int rv;
+	struct usb_driver *subdriver = NULL;
+	atomic_t *pmcount = (void *)&dev->data[1];
+	if (dev->driver_info->data &&
+	!test_bit(intf->cur_altsetting->desc.bInterfaceNumber, &dev->driver_info->data)) {
+		dev_info(&intf->dev, "not on our whitelist - ignored");
+		rv = -ENODEV;
+		goto err;
+	}
+	atomic_set(pmcount, 0);
+	/* collect all three endpoints */
+	rv = usbnet_get_endpoints(dev, intf);
+	if (rv < 0)
+		goto err;
+	/* require interrupt endpoint for subdriver */
+	if (!dev->status) {
+		rv = -EINVAL;
+		goto err;
+	}
+	subdriver = usb_cdc_wdm_register(intf, &dev->status->desc, 512,
+	&qmi_wwan_cdc_wdm_manage_power);
+	if (IS_ERR(subdriver)) {
+		rv = PTR_ERR(subdriver);
+		goto err;
+	}
+	/* can't let usbnet use the interrupt endpoint */
+	dev->status = NULL;
+	/* save subdriver struct for suspend/resume wrappers */
+	dev->data[0] = (unsigned long)subdriver;
+#if 1
+	if ((dev->udev->descriptor.idVendor == cpu_to_le16(0x05c6))) {
+		dev_info(&intf->dev, "yuge 9x07 work on RawIP mode\n");
+		dev->net->flags |= IFF_NOARP;
+	//#if (LINUX_VERSION_CODE < KERNEL_VERSION( 3,9,1 ))
+	#if 0
+		/* make MAC addr easily distinguishable from an IP header */
+		if (possibly_iphdr(dev->net->dev_addr)) {
+			dev->net->dev_addr[0] |= 0x02; /* set local assignment bit */
+			dev->net->dev_addr[0] &= 0xbf; /* clear "IP" bit */
+		}
+	#endif
+		usb_control_msg(
+			interface_to_usbdev(intf),
+			usb_sndctrlpipe(interface_to_usbdev(intf), 0),
+			0x22, //USB_CDC_REQ_SET_CONTROL_LINE_STATE
+			0x21, //USB_DIR_OUT | USB_TYPE_CLASS | USB_RECIP_INTERFACE
+			1, //active CDC DTR
+			intf->cur_altsetting->desc.bInterfaceNumber,
+			NULL, 0, 100);
+}
+#endif
+err:
+	return rv;
+}
+
+struct sk_buff *qmi_wwan_tx_fixup_YUGA(struct usbnet *dev, struct sk_buff *skb, gfp_t
+flags)
+{
+	if (dev->udev->descriptor.idVendor != cpu_to_le16(0x05c6))
+		return skb;
+	// Skip Ethernet header from message
+	if (skb_pull(skb, ETH_HLEN)) {
+		return skb;
+	} else {
+		dev_err(&dev->intf->dev, "Packet Dropped ");
+	}
+	// Filter the packet out, release it
+	dev_kfree_skb_any(skb);
+	return NULL;
+}
+
+static int qmi_wwan_rx_fixup_YUGA(struct usbnet *dev, struct sk_buff *skb)
+{
+	__be16 proto;
+	if (dev->udev->descriptor.idVendor != cpu_to_le16(0x05c6))
+		return 1;
+	/* This check is no longer done by usbnet */
+	if (skb->len < dev->net->hard_header_len)
+		return 0;
+	switch (skb->data[0] & 0xf0) {
+	case 0x40:
+		proto = htons(ETH_P_IP);
+		break;
+	case 0x60:
+		proto = htons(ETH_P_IPV6);
+		break;
+	case 0x00:
+		if (is_multicast_ether_addr(skb->data))
+			return 1;
+	/* possibly bogus destination - rewrite just in case */
+		skb_reset_mac_header(skb);
+		goto fix_dest;
+	default:
+		/* pass along other packets without modifications */
+		return 1;
+	}
+	if (skb_headroom(skb) < ETH_HLEN)
+		return 0;
+	skb_push(skb, ETH_HLEN);
+	skb_reset_mac_header(skb);
+	eth_hdr(skb)->h_proto = proto;
+	memset(eth_hdr(skb)->h_source, 0, ETH_ALEN);
 fix_dest:
 	memcpy(eth_hdr(skb)->h_dest, dev->net->dev_addr, ETH_ALEN);
 	return 1;
@@ -526,6 +637,25 @@ static const struct driver_info	qmi_wwan_info = {
 	.rx_fixup       = qmi_wwan_rx_fixup,
 };
 
+static void qmi_wwan_unbind_shared(struct usbnet *dev, struct usb_interface *intf)
+{
+	struct usb_driver *subdriver = (void *)dev->data[0];
+	if (subdriver && subdriver->disconnect)
+		subdriver->disconnect(intf);
+	dev->data[0] = (unsigned long)NULL;
+}
+
+static const struct driver_info qmi_wwan_force_int4 = {
+	.description= "Qualcomm WWAN/QMI device",
+	.flags= FLAG_WWAN,
+	.bind= qmi_wwan_bind_shared,
+	.unbind= qmi_wwan_unbind_shared,
+	.manage_power= qmi_wwan_manage_power,
+	.data= BIT(4), /* interface whitelist bitmap */
+	.tx_fixup = qmi_wwan_tx_fixup_YUGA,
+	.rx_fixup = qmi_wwan_rx_fixup_YUGA,
+};
+
 #define HUAWEI_VENDOR_ID	0x12D1
 
 /* map QMI/wwan function by a fixed interface number */
@@ -540,6 +670,17 @@ static const struct driver_info	qmi_wwan_info = {
 /* Gobi 2000/3000 QMI/wwan interface number is 0 according to qcserial */
 #define QMI_GOBI_DEVICE(vend, prod) \
 	QMI_FIXED_INTF(vend, prod, 0)
+
+#ifndef QMI_FIXED_INTF_YUGA
+#define QMI_FIXED_INTF_YUGA(vendor,prod,num)\
+.match_flags=USB_DEVICE_ID_MATCH_DEVICE | USB_DEVICE_ID_MATCH_INT_INFO,\
+.idVendor =vendor,\
+.idProduct =prod, \
+.bInterfaceClass =0xff,\
+.bInterfaceSubClass=0xff,\
+.bInterfaceProtocol=0xff,\
+.driver_info =(unsigned long )&qmi_wwan_force_int##num,
+#endif
 
 static const struct usb_device_id products[] = {
 	/* 1. CDC ECM like devices match on the control interface */
@@ -679,6 +820,7 @@ static const struct usb_device_id products[] = {
 	{QMI_FIXED_INTF(0x05c6, 0x9021, 1)},
 	{QMI_FIXED_INTF(0x05c6, 0x9022, 2)},
 	{QMI_FIXED_INTF(0x05c6, 0x9025, 4)},	/* Alcatel-sbell ASB TL131 TDD LTE  (China Mobile) */
+	{QMI_FIXED_INTF_YUGA(0x05c6, 0x9625, 4)},
 	{QMI_FIXED_INTF(0x05c6, 0x9026, 3)},
 	{QMI_FIXED_INTF(0x05c6, 0x902e, 5)},
 	{QMI_FIXED_INTF(0x05c6, 0x9031, 5)},

DorianPio/linux-4.4

drivers/net/usb/qmi_wwan.c
@@ -63,6 +63,8 @@ enum qmi_wwan_quirks {
 	QMI_WWAN_QUIRK_DTR = 1 << 0,	/* needs "set DTR" request */
 };
 
+static int qmi_wwan_cdc_wdm_manage_power(struct usb_interface *intf, int on);
+
 static void qmi_wwan_netdev_setup(struct net_device *net)
 {
 	struct usbnet *dev = netdev_priv(net);
@@ -216,6 +218,114 @@ static int qmi_wwan_rx_fixup(struct usbnet *dev, struct sk_buff *skb)
 	skb_reset_mac_header(skb);
 	eth_hdr(skb)->h_proto = proto;
 	eth_zero_addr(eth_hdr(skb)->h_source);
+fix_dest:
+	memcpy(eth_hdr(skb)->h_dest, dev->net->dev_addr, ETH_ALEN);
+	return 1;
+}
+
+static int qmi_wwan_bind_shared(struct usbnet *dev, struct usb_interface *intf)
+{
+	int rv;
+	struct usb_driver *subdriver = NULL;
+	atomic_t *pmcount = (void *)&dev->data[1];
+	if (dev->driver_info->data &&
+	!test_bit(intf->cur_altsetting->desc.bInterfaceNumber, &dev->driver_info->data)) {
+		dev_info(&intf->dev, "not on our whitelist - ignored");
+		rv = -ENODEV;
+		goto err;
+	}
+	atomic_set(pmcount, 0);
+	/* collect all three endpoints */
+	rv = usbnet_get_endpoints(dev, intf);
+	if (rv < 0)
+		goto err;
+	/* require interrupt endpoint for subdriver */
+	if (!dev->status) {
+		rv = -EINVAL;
+		goto err;
+	}
+	subdriver = usb_cdc_wdm_register(intf, &dev->status->desc, 512,
+	&qmi_wwan_cdc_wdm_manage_power);
+	if (IS_ERR(subdriver)) {
+		rv = PTR_ERR(subdriver);
+		goto err;
+	}
+	/* can't let usbnet use the interrupt endpoint */
+	dev->status = NULL;
+	/* save subdriver struct for suspend/resume wrappers */
+	dev->data[0] = (unsigned long)subdriver;
+
+	if ((dev->udev->descriptor.idVendor == cpu_to_le16(0x05c6))) {
+		dev_info(&intf->dev, "yuge 9x07 work on RawIP mode\n");
+		dev->net->flags |= IFF_NOARP;
+		usb_control_msg(
+			interface_to_usbdev(intf),
+			usb_sndctrlpipe(interface_to_usbdev(intf), 0),
+			0x22, //USB_CDC_REQ_SET_CONTROL_LINE_STATE
+			0x21, //USB_DIR_OUT | USB_TYPE_CLASS | USB_RECIP_INTERFACE
+			1, //active CDC DTR
+			intf->cur_altsetting->desc.bInterfaceNumber,
+			NULL, 0, 100);
+	}
+err:
+	return rv;
+}
+
+static void qmi_wwan_unbind_shared(struct usbnet *dev, struct usb_interface *intf)
+{
+	struct usb_driver *subdriver = (void *)dev->data[0];
+	if (subdriver && subdriver->disconnect)
+		subdriver->disconnect(intf);
+	dev->data[0] = (unsigned long)NULL;
+}
+
+struct sk_buff *qmi_wwan_tx_fixup_YUGA(struct usbnet *dev, struct sk_buff *skb, gfp_t
+flags)
+{
+	if (dev->udev->descriptor.idVendor != cpu_to_le16(0x05c6))
+		return skb;
+	// Skip Ethernet header from message
+	if (skb_pull(skb, ETH_HLEN)) {
+		return skb;
+	} else {
+		dev_err(&dev->intf->dev, "Packet Dropped ");
+	}
+	// Filter the packet out, release it
+	dev_kfree_skb_any(skb);
+	return NULL;
+}
+
+static int qmi_wwan_rx_fixup_YUGA(struct usbnet *dev, struct sk_buff *skb)
+{
+	__be16 proto;
+	if (dev->udev->descriptor.idVendor != cpu_to_le16(0x05c6))
+		return 1;
+	/* This check is no longer done by usbnet */
+	if (skb->len < dev->net->hard_header_len)
+		return 0;
+	switch (skb->data[0] & 0xf0) {
+	case 0x40:
+		proto = htons(ETH_P_IP);
+		break;
+	case 0x60:
+		proto = htons(ETH_P_IPV6);
+		break;
+	case 0x00:
+		if (is_multicast_ether_addr(skb->data))
+			return 1;
+	/* possibly bogus destination - rewrite just in case */
+		skb_reset_mac_header(skb);
+		goto fix_dest;
+	default:
+		/* pass along other packets without modifications */
+		return 1;
+	}
+	if (skb_headroom(skb) < ETH_HLEN)
+		return 0;
+	skb_push(skb, ETH_HLEN);
+	skb_reset_mac_header(skb);
+	eth_hdr(skb)->h_proto = proto;
+	memset(eth_hdr(skb)->h_source, 0, ETH_ALEN);
 fix_dest:
 	memcpy(eth_hdr(skb)->h_dest, dev->net->dev_addr, ETH_ALEN);
 	return 1;
@@ -545,6 +655,17 @@ static const struct driver_info	qmi_wwan_info_quirk_dtr = {
 	.data           = QMI_WWAN_QUIRK_DTR,
 };
 
+static const struct driver_info qmi_wwan_force_int4 = {
+	.description    = "Qualcomm WWAN/QMI device",
+	.flags          = FLAG_WWAN,
+	.bind           = qmi_wwan_bind_shared,
+	.unbind         = qmi_wwan_unbind_shared,
+	.manage_power   = qmi_wwan_manage_power,
+	.tx_fixup       = qmi_wwan_tx_fixup_YUGA,
+	.rx_fixup       = qmi_wwan_rx_fixup_YUGA,
+	.data           = BIT(4), /* interface whitelist bitmap */
+};
+
 #define HUAWEI_VENDOR_ID	0x12D1
 
 /* map QMI/wwan function by a fixed interface number */
@@ -564,6 +685,10 @@ static const struct driver_info	qmi_wwan_info_quirk_dtr = {
 /* Gobi 2000/3000 QMI/wwan interface number is 0 according to qcserial */
 #define QMI_GOBI_DEVICE(vend, prod) \
 	QMI_FIXED_INTF(vend, prod, 0)
+
+#define QMI_FIXED_INTF_YUGA(vend, prod, num) \
+	USB_DEVICE_AND_INTERFACE_INFO(vend, prod, 0xff, 0xff, 0xff), \
+	.driver_info = (unsigned long )&qmi_wwan_force_int##num
 
 static const struct usb_device_id products[] = {
 	/* 1. CDC ECM like devices match on the control interface */
@@ -703,6 +828,7 @@ static const struct usb_device_id products[] = {
 	{QMI_FIXED_INTF(0x05c6, 0x9021, 1)},
 	{QMI_FIXED_INTF(0x05c6, 0x9022, 2)},
 	{QMI_QUIRK_SET_DTR(0x05c6, 0x9025, 4)},	/* Alcatel-sbell ASB TL131 TDD LTE (China Mobile) */
+	{QMI_FIXED_INTF_YUGA(0x05c6, 0x9625, 4)},
 	{QMI_FIXED_INTF(0x05c6, 0x9026, 3)},
 	{QMI_FIXED_INTF(0x05c6, 0x902e, 5)},
 	{QMI_FIXED_INTF(0x05c6, 0x9031, 5)},